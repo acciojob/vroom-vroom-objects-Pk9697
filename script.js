96f66a420e519ea78b2a78a57624a099f3b8cb4f
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-15 18:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Complete the js code
-function Car(make, model) {
-	this._make=make
-	this._model=model
-}
+// function Car(make, model) {
+// 	this._make=make
+// 	this._model=model
+// }
 
-Car.prototype.getMakeModel=function () {
-	return `${this._make} ${this._model}`
-}
+// Car.prototype.getMakeModel=function () {
+// 	return `${this._make} ${this._model}`
+// }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model)
-	this._topSpeed=topSpeed
-}
-// Shallow copy to copy all the methods Car has like getMakeModel , 
-// and doing this in shallow copy way so that any 
-SportsCar.prototype={...Car.prototype}
-// SportsCar.prototype=Object.create({},Car.prototype)
+// function SportsCar(make, model, topSpeed) {
+// 	Car.call(this,make,model)
+// 	this._topSpeed=topSpeed
+// }
+// // Shallow copy to copy all the methods Car has like getMakeModel , 
+// // and doing this in shallow copy way so that any 
+// SportsCar.prototype={...Car.prototype}
+// // SportsCar.prototype=Object.create({},Car.prototype)
 
-SportsCar.prototype.getTopSpeed=function () {
-	return this._topSpeed
-}
+// SportsCar.prototype.getTopSpeed=function () {
+// 	return this._topSpeed
+// }
  
 // const car = new SportsCar("Ferrari", "Testarossa", 200);
 // console.log(car.getMakeModel()); // Output: Ferrari Testarossa
@@ -31,6 +31,28 @@ SportsCar.prototype.getTopSpeed=function () {
 // }
 
 // console.log(car.getInfo());
+
+class Car{
+	constructor(make,model){
+		this._make=make
+		this._model=model
+	}
+
+	getMakeModel() {
+		return `${this._make} ${this._model}`
+	}
+}
+
+class SportsCar extends Car{
+	constructor(make,model,topSpeed){
+		super(make,model)
+		this._topSpeed=topSpeed
+	}
+
+	getTopSpeed() {
+		return this._topSpeed
+	}
+}
 
 
 // Do not change the code below
