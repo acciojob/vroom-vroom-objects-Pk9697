0dbc665aed02bc8723e34dfcf529f45756f1a8a9
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-15 18:23:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this._make=make
+	this._model=model
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel=function () {
+	return `${this._make} ${this._model}`
+}
+
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model)
+	this._topSpeed=topSpeed
+}
+// Shallow copy to copy all the methods Car has like getMakeModel , 
+// and doing this in shallow copy way so that any 
+SportsCar.prototype={...Car.prototype}
+// SportsCar.prototype=Object.create({},Car.prototype)
+
+SportsCar.prototype.getTopSpeed=function () {
+	return this._topSpeed
+}
+ 
+// const car = new SportsCar("Ferrari", "Testarossa", 200);
+// console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+// console.log(car.getTopSpeed()); // Output: 200
+
+
+// SportsCar.prototype.getInfo=function () {
+// 	return `${this._make} + ${this._model}`
+// }
+
+// console.log(car.getInfo());
+
 
 // Do not change the code below
 window.Car = Car;
